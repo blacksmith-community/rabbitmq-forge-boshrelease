cbc62a8635f62f5f3ce34537d3b53a907cd789b9
Wayne E. Seguin <wayneeseguin@gmail.com>
2025-08-17 16:31:32 -0400

    Migrated from Monit to BPM.
    Cleaned up cruft left over.

--- jobs/rabbitmq/templates/env.rb
+++ jobs/rabbitmq/templates/env.rb
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # BOSH Env Vars
-export HOME=${HOME:-/home/vcap}
-export JOB_DIR=/var/vcap/jobs/rabbitmq
-export RUN_DIR=/var/vcap/sys/run/rabbitmq
-export LOG_DIR=/var/vcap/sys/log/rabbitmq
-export TMP_DIR=/var/vcap/sys/tmp/rabbitmq
-export STORE_DIR=/var/vcap/store/rabbitmq
+export HOME="${HOME:-/home/vcap}"
+export JOB_DIR="/var/vcap/jobs/rabbitmq"
+export RUN_DIR="/var/vcap/sys/run/rabbitmq"
+export LOG_DIR="/var/vcap/sys/log/rabbitmq"
+export TMP_DIR="/var/vcap/sys/tmp/rabbitmq"
+export STORE_DIR="/var/vcap/store/rabbitmq"
 
 # RabbitMQ
 export RABBITMQ_PLAN="<%= p('rabbitmq.plan') %>"
@@ -16,9 +16,9 @@ export RABBITMQ_MONITORING_PASS="<%= p('rabbitmq.monitoring.pass') %>"
 export RABBITMQ_MONITORING_USER="<%= p('rabbitmq.monitoring.user') %>"
 export RABBITMQ_APP_PASS="<%= p('rabbitmq.app.pass') %>"
 export RABBITMQ_APP_USER="<%= p('rabbitmq.app.user') %>"
-export RABBITMQ_CONFIG_FILE=${JOB_DIR}/config/rabbitmq.conf
-export RABBITMQ_LOG_BASE=${LOG_DIR}
-export RABBITMQ_MNESIA_BASE=/var/vcap/store/rabbitmq
+export RABBITMQ_CONFIG_FILE="${JOB_DIR}/config/rabbitmq.conf"
+export RABBITMQ_LOG_BASE="${LOG_DIR}"
+export RABBITMQ_MNESIA_BASE="/var/vcap/store/rabbitmq"
 export RABBITMQ_NODENAME="rabbit@<%= spec.id %>.<%= spec.name %>.<%= p('rabbitmq.network').gsub('.', '') %>.<%= spec.deployment %>.bosh"
 export RABBITMQ_PID_FILE="${RUN_DIR}/rabbitmq.pid"
 export RABBITMQ_USE_LONGNAME="true"
@@ -27,7 +27,7 @@ export RABBITMQ_EVAL_TIMEOUT="90"
 
 # Erlang
 export EPMD_PID_FILE="${RUN_DIR}/epmd.pid"
-export ERL_INETRC=${JOB_DIR}/config/erl_inetrc
+export ERL_INETRC="${JOB_DIR}/config/erl_inetrc"
 
 # Elixir expects utf8, locale must be set to UTF-8.
 export LC_ALL=en_US.UTF-8
@@ -35,5 +35,5 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
 # Erlang & RabbitMQ Binaries
-export PATH=$PATH:/var/vcap/packages/erlang/bin:/var/vcap/packages/rabbitmq/sbin
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/var/vcap/packages/erlang/bin:/var/vcap/packages/rabbitmq/sbin:${PATH}"
 
